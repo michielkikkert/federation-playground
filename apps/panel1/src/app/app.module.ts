628380f5dba9d8083f765acd2c95213184a1465e
Michiel Kikkert <michiel@kikkert.nl>
2021-08-31 16:46:49 +0200

    Partial Cleanup reversal... was a bit too rigorous ;-)

--- apps/panel1/src/app/app.module.ts
+++ apps/panel1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { RemoteEntryComponent } from './remote-entry/entry.component';
+import { RemoteEntryModule } from './remote-entry/entry.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    RemoteEntryModule,
     RouterModule.forRoot([{
         path: '',
         component: RemoteEntryComponent
