d9d4b65167bce6ca66f939ec172aaf5fae57b9d1
Michiel Kikkert <michiel@kikkert.nl>
2021-08-31 17:20:08 +0200

    Fetchin a (remote) config - initial fetch test.

--- apps/hostapp/src/bootstrap.ts
+++ apps/hostapp/src/bootstrap.ts
@@ -8,6 +8,12 @@ if (environment.production) {
   enableProdMode();
 }
 
+fetch('http://127.0.0.1:8080/config.json').then(
+    response => response.json()
+    ).then (
+        data => console.log(data)
+);
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
