b1d41b3fe2122733d8386497e416520c2e2f5c8b
Michiel Kikkert <michiel@kikkert.nl>
2021-09-01 15:36:30 +0200

    Cleanup. Better typing.

--- apps/hostapp/src/bootstrap.ts
+++ apps/hostapp/src/bootstrap.ts
@@ -4,6 +4,7 @@ import { loadRemoteModule } from '@angular-architects/module-federation'
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { MODULES_CONFIG } from './moduleinjection.token';
+import { ModuleConfig } from './moduleconfig.model';
 
 if (environment.production) {
   enableProdMode();
@@ -11,7 +12,7 @@ if (environment.production) {
 
 async function main() {
     const response = await fetch('http://127.0.0.1:8080/config.json');
-    const config = await response.json() as any;
+    const config = await response.json() as ModuleConfig;
     const namespace = 'moduleConfig';
 
     (window as any)[namespace] = config;
