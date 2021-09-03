8a62f0f60b5b0bfd623b9ec99c691aaadefc7693
Michiel Kikkert <michiel@kikkert.nl>
2021-09-03 17:15:49 +0200

    Also added in sharedMappings.register

--- apps/panel1/src/app/app.module.ts
+++ apps/panel1/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ComponentFactory, ComponentFactoryResolver, NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RemoteEntryModule } from './remote-entry/entry.module';
@@ -7,16 +7,9 @@ import { RemoteEntryModule } from './remote-entry/entry.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    RemoteEntryModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
-
-  public resolveComponent(): ComponentFactory<AppComponent> {
-    return this.componentFactoryResolver.resolveComponentFactory(AppComponent);
-  }
-}
+export class AppModule {}
